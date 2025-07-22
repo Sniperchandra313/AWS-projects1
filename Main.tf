f23e1f7112707731f56a1cd5d62e63b654815ba6
Sniperchandra313 <107684507+Sniperchandra313@users.noreply.github.com>
2025-07-23 00:38:45 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"  # Change region as needed
-  access_key = "AKIA4LQJRLXSVKKDZLIJ"
-  secret_key = "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"
+  access_key = ""
+  secret_key = "NA"
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
