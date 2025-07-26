49977ed3b2ae3268c37f6f4095b8fde8c1ae3203
Sniperchandra313 <107684507+Sniperchandra313@users.noreply.github.com>
2025-07-27 00:19:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
  
 }
 
-resource "aws_iam_role" "lambda_exec_role1" {
-  name = "lambda_exec_role1"
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
 
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
